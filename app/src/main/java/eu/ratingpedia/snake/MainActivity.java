3582feaa8a7db4d6d654c7185e09784938e8416e
marcinwal <marcin.walendzik@gmail.com>
2015-06-13 22:09:02 +0100

    satring with SnakeAnim class Runnable

--- app/src/main/java/eu/ratingpedia/snake/MainActivity.java
+++ app/src/main/java/eu/ratingpedia/snake/MainActivity.java
@@ -1,12 +1,18 @@
 package eu.ratingpedia.snake;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Bundle;
-
+import android.view.Display;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 
 public class MainActivity extends Activity {
@@ -35,7 +41,59 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
+
+        headAnimBitmap = BitmapFactory.decodeResource(getResources(),
+                         R.drawable.head_sprite_sheet);
+
+        snakeAnimView = new SnakeAnimView(this);
+        setContentView(snakeAnimView);
+
+        i = new Intent(this,GameActivity.class);
+
+        class SnakeAnimView extends SurfaceView implements Runnable{
+
+            Thread ourThread = null;
+            SurfaceHolder ourHolder;
+            volatile boolean playingSnake;
+            Paint paint;
+
+            public SnakeAnimView(Context context){
+                super(context);
+                ourHolder = getHolder();
+                paint = new Paint();
+                frameWidth = headAnimBitmap.getWidth()/numFrames;
+                frameHeight = headAnimBitmap.getHeight();
+            }
+
+            @Override
+            public void run() {
+                while(playingSnake){
+                    update();
+                    draw();
+                    controlFPS();
+                }
+            }
+
+            private void update() {
+                rectToBeDrawn = new Rect((frameNumber * frameWidth)-1),
+                                0,(frameNumber * frameWidth + frameWidth)-1,
+                                frameHeight);
+
+                frameNumber++;
+                if(frameNumber == numFrames){
+                    frameNumber = 0;
+                }
+            }
+        }
+
+
+
+
 
 
     }
